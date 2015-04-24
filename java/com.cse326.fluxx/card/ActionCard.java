722225a83e92c1bb72d3694211ec8345cdce6eff
Mista-Tea <ryan_zauros@hotmail.com>
2015-04-24 04:10:34 -0600

    Update ActionCard.java

--- java/com.cse326.fluxx/card/ActionCard.java
+++ java/com.cse326.fluxx/card/ActionCard.java
@@ -1,9 +1,10 @@
 package com.cse326.fluxx.card;
 
+import android.media.Image;
 /**
  * Created by Thomas on 4/24/2015.
  */
-public class ActionCard extends Card {
+public class ActionCard extends FluxxCard {
 
 	/* ------------------------------------ Instance Variables ---------------------------------- */
 
@@ -15,15 +16,43 @@ public class ActionCard extends Card {
 	 *
 	 */
 	public ActionCard() {
-
+		super();
+		super.setType( CARD_TYPE.ACTION );
 	}
 
+	/**
+	 *
+	 * @param name
+	 */
+	public ActionCard( String name ) {
+		super( name );
+		super.setType( CARD_TYPE.ACTION );
+	}
+
+	/**
+	 *
+	 * @param name
+	 * @param use
+	 * @param effect
+	 * @param image
+	 */
+	protected ActionCard( String name, String use, String effect, Image image ) {
+		super( name, use, effect, image );
+		super.setType( CARD_TYPE.ACTION );
+	}
 
 	/* ------------------------------------------- Methods -------------------------------------- */
 
+	@Override
+	public void play() {
 
+	}
+
+	@Override
+	public void discard() {
+
+	}
 
 	/* ------------------------------------- Getters & Setters ---------------------------------- */
-
 
 }
