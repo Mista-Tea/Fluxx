5560ff280bde47e17bd84663553694ee7bda3f87
Mista-Tea <ryan_zauros@hotmail.com>
2015-04-24 04:11:08 -0600

    Update RuleCard.java

--- java/com.cse326.fluxx/card/RuleCard.java
+++ java/com.cse326.fluxx/card/RuleCard.java
@@ -1,9 +1,10 @@
 package com.cse326.fluxx.card;
 
+import android.media.Image;
 /**
  * Created by Thomas on 4/24/2015.
  */
-public class RuleCard extends Card {
+public class RuleCard extends FluxxCard {
 
 	/* ------------------------------------ Instance Variables ---------------------------------- */
 
@@ -15,14 +16,43 @@ public class RuleCard extends Card {
 	 *
 	 */
 	public RuleCard() {
+		super();
+		super.setType( CARD_TYPE.RULE );
+	}
 
+	/**
+	 *
+	 * @param name
+	 */
+	public RuleCard( String name ) {
+		super( name );
+		super.setType( CARD_TYPE.RULE );
+	}
+
+	/**
+	 *
+	 * @param name
+	 * @param use
+	 * @param effect
+	 * @param image
+	 */
+	protected RuleCard( String name, String use, String effect, Image image ) {
+		super( name, use, effect, image );
+		super.setType( CARD_TYPE.RULE );
 	}
 
 	/* ------------------------------------------- Methods -------------------------------------- */
 
+	@Override
+	public void play() {
 
+	}
+
+	@Override
+	public void discard() {
+
+	}
 
 	/* ------------------------------------- Getters & Setters ---------------------------------- */
-
 
 }
