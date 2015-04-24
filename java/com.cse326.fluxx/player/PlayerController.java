9d8e3f997d7592b8731cb210be9b9831f5831bdc
Mista-Tea <ryan_zauros@hotmail.com>
2015-04-24 02:17:48 -0600

    Create PlayerController.java

---
+++ java/com.cse326.fluxx/player/PlayerController.java
@@ -0,0 +1,30 @@
+package com.cse326.fluxx.player;
+
+/**
+ * Created by Thomas on 4/24/2015.
+ *
+ * The player controller manages the player inputs into the game.
+ * Whenever a player wants to play a card they first interact with the view and then select a card to play.
+ * If any actions need more input from the player the player controller will handle those extra inputs.
+ */
+public class PlayerController {
+
+	/* ------------------------------------ Instance Variables ---------------------------------- */
+
+
+
+	/* ---------------------------------------- Constructors ------------------------------------ */
+
+	public PlayerController() {
+
+	}
+
+	/* ------------------------------------------- Methods -------------------------------------- */
+
+
+
+	/* ------------------------------------- Getters & Setters ---------------------------------- */
+
+
+
+}
