340e9b30eac5d476b0bed7524375dd066a8998d8
Mista-Tea <ryan_zauros@hotmail.com>
2015-04-24 04:10:56 -0600

    Update KeeperCard.java

--- java/com.cse326.fluxx/card/KeeperCard.java
+++ java/com.cse326.fluxx/card/KeeperCard.java
@@ -1,9 +1,10 @@
 package com.cse326.fluxx.card;
 
+import android.media.Image;
 /**
  * Created by Thomas on 4/24/2015.
  */
-public class KeeperCard extends Card {
+public class KeeperCard extends FluxxCard {
 
 	/* ------------------------------------ Instance Variables ---------------------------------- */
 
@@ -15,14 +16,43 @@ public class KeeperCard extends Card {
 	 *
 	 */
 	public KeeperCard() {
+		super();
+		super.setType( CARD_TYPE.KEEPER );
+	}
 
+	/**
+	 *
+	 * @param name
+	 */
+	public KeeperCard( String name ) {
+		super( name );
+		super.setType( CARD_TYPE.KEEPER );
+	}
+
+	/**
+	 *
+	 * @param name
+	 * @param use
+	 * @param effect
+	 * @param image
+	 */
+	protected KeeperCard( String name, String use, String effect, Image image ) {
+		super( name, use, effect, image );
+		super.setType( CARD_TYPE.KEEPER );
 	}
 
 	/* ------------------------------------------- Methods -------------------------------------- */
 
+	@Override
+	public void play() {
 
+	}
+
+	@Override
+	public void discard() {
+
+	}
 
 	/* ------------------------------------- Getters & Setters ---------------------------------- */
-
 
 }
