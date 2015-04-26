1e26b575c2ec6d3549d6de0e99eecde09b8ae4be
Mista-Tea <ryan_zauros@hotmail.com>
2015-04-25 22:32:35 -0600

    Create Human.java

---
+++ java/com.cse326.fluxx/player/Human.java
@@ -0,0 +1,23 @@
+package fluxx.player;
+
+import java.util.ArrayList;
+
+import fluxx.card.Card;
+/**
+ * Created by Thomas on 4/25/2015.
+ */
+public class Human implements Player {
+
+	public Player choosePlayer( ArrayList<Player> players ) {
+		// TODO
+		// return this.menu.getPlayerSelection();
+		return players.get( (int) Math.random() * players.size() );
+	}
+
+	public Card chooseCard( ArrayList<Card> cards ) {
+		// TODO
+		// return this.menu.getCardSelection();
+		return cards.get( (int) Math.random() * cards.size() );
+	}
+
+}
