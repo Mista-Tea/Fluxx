6d12c701eba333efabd08f73c7b7d4a7600c3486
Mista-Tea <ryan_zauros@hotmail.com>
2015-04-25 22:32:21 -0600

    Create Bot.java

---
+++ java/com.cse326.fluxx/player/Bot.java
@@ -0,0 +1,19 @@
+package fluxx.player;
+
+import java.util.ArrayList;
+
+import fluxx.card.Card;
+/**
+ * Created by Thomas on 4/25/2015.
+ */
+public class Bot implements Player {
+
+	public Player choosePlayer( ArrayList<Player> players ) {
+		return players.get( (int) Math.random() * players.size() );
+	}
+
+	public Card chooseCard( ArrayList<Card> cards ) {
+		return cards.get( (int) Math.random() * cards.size() );
+	}
+
+}
